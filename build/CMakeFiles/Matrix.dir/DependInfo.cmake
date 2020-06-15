203b0b3170ac990f3161fc477b683f6f8003cba9
Paweł Dyrda <01149820@pw.edu.pl>
2020-06-15 23:37:39 +0200

    switched from SCons to CMake

---
+++ build/CMakeFiles/Matrix.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/command.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/command.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/dataConverter.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/dataConverter.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/exceptions.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/exceptions.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/inputAnalyzer.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/inputAnalyzer.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/main.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/main.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/matrix.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/matrix.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/matrixVector.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/matrixVector.cpp.o"
+  "/home/paweld/Dokumenty/C++/lepszy_kalkulator/src/menu.cpp" "/home/paweld/Dokumenty/C++/lepszy_kalkulator/build/CMakeFiles/Matrix.dir/src/menu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matrix.dir/src/command.cpp.o"
+  "CMakeFiles/Matrix.dir/src/dataConverter.cpp.o"
+  "CMakeFiles/Matrix.dir/src/exceptions.cpp.o"
+  "CMakeFiles/Matrix.dir/src/inputAnalyzer.cpp.o"
+  "CMakeFiles/Matrix.dir/src/main.cpp.o"
+  "CMakeFiles/Matrix.dir/src/matrix.cpp.o"
+  "CMakeFiles/Matrix.dir/src/matrixVector.cpp.o"
+  "CMakeFiles/Matrix.dir/src/menu.cpp.o"
+  "Matrix"
+  "Matrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
